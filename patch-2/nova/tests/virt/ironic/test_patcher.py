d4608327f9f56ebfb2626841344382e72f7f871f
Michael Davies <michael@the-davies.net>
2014-07-31 04:17:35 +0930

    Initial version and split-up

---
+++ patch-2/nova/tests/virt/ironic/test_patcher.py
@@ -0,0 +1,135 @@
+# Copyright 2014 Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo.config import cfg
+
+from nova import context as nova_context
+from nova import test
+from nova.tests import fake_instance
+from nova.tests.virt.ironic import utils as ironic_utils
+from nova.virt.ironic import patcher
+
+CONF = cfg.CONF
+
+
+class IronicDriverFieldsTestCase(test.NoDBTestCase):
+
+    def setUp(self):
+        super(IronicDriverFieldsTestCase, self).setUp()
+        self.image_meta = ironic_utils.get_test_image_meta()
+        self.flavor = ironic_utils.get_test_flavor()
+        self.ctx = nova_context.get_admin_context()
+
+    def test_pxe_get_deploy_patch(self):
+        node = ironic_utils.get_test_node(driver='pxe_fake')
+        instance = fake_instance.fake_instance_obj(self.ctx, node=node.uuid)
+        extra_specs = self.flavor['extra_specs']
+        expected = [{'path': '/driver_info/pxe_deploy_kernel',
+                     'value': extra_specs['baremetal:deploy_kernel_id'],
+                     'op': 'add'},
+                    {'path': '/driver_info/pxe_deploy_ramdisk',
+                     'value': extra_specs['baremetal:deploy_ramdisk_id'],
+                     'op': 'add'},
+                    {'path': '/instance_info/image_source',
+                     'value': self.image_meta['id'],
+                     'op': 'add'},
+                    {'path': '/instance_info/root_gb',
+                     'value': str(instance['root_gb']),
+                     'op': 'add'},
+                    {'path': '/instance_info/swap_mb',
+                     'value': str(self.flavor['swap']),
+                     'op': 'add'}]
+        patch = patcher.create(node).get_deploy_patch(
+                instance, self.image_meta, self.flavor)
+        self.assertEqual(sorted(expected), sorted(patch))
+
+    def test_pxe_get_deploy_patch_with_ephemeral(self):
+        node = ironic_utils.get_test_node(driver='pxe_fake')
+        instance = fake_instance.fake_instance_obj(
+                        self.ctx, node=node.uuid, ephemeral_gb=10)
+        CONF.set_override('default_ephemeral_format', 'testfmt')
+        patch = patcher.create(node).get_deploy_patch(
+                instance, self.image_meta, self.flavor)
+        expected1 = {'path': '/instance_info/ephemeral_gb',
+                     'value': '10', 'op': 'add'}
+        expected2 = {'path': '/instance_info/ephemeral_format',
+                     'value': 'testfmt', 'op': 'add'}
+        self.assertIn(expected1, patch)
+        self.assertIn(expected2, patch)
+
+    def test_pxe_get_deploy_patch_preserve_ephemeral(self):
+        node = ironic_utils.get_test_node(driver='pxe_fake')
+        instance = fake_instance.fake_instance_obj(
+                        self.ctx, node=node.uuid, ephemeral_gb=10)
+        for preserve in [True, False]:
+            patch = patcher.create(node).get_deploy_patch(
+                    instance, self.image_meta, self.flavor,
+                    preserve_ephemeral=preserve)
+            expected = {'path': '/instance_info/preserve_ephemeral',
+                        'value': str(preserve), 'op': 'add', }
+            self.assertIn(expected, patch)
+
+    def test_pxe_get_deploy_patch_no_preserve_ephemeral(self):
+        node = ironic_utils.get_test_node(driver='pxe_fake')
+        instance = fake_instance.fake_instance_obj(
+                        self.ctx, node=node.uuid, ephemeral_gb=10)
+        patch = patcher.create(node).get_deploy_patch(
+                    instance, self.image_meta, self.flavor)
+        for preserve in [True, False]:
+            unexpected = {'path': '/instance_info/preserve_ephemeral',
+                          'value': str(preserve), 'op': 'add', }
+            self.assertNotIn(unexpected, patch)
+
+    def test_pxe_get_deploy_patch_no_flavor_kernel_ramdisk_ids(self):
+        self.flavor = ironic_utils.get_test_flavor(extra_specs={})
+        node = ironic_utils.get_test_node(driver='pxe_fake')
+        instance = fake_instance.fake_instance_obj(self.ctx, node=node.uuid)
+        expected = [{'path': '/instance_info/image_source',
+                     'value': self.image_meta['id'],
+                     'op': 'add'},
+                    {'path': '/instance_info/root_gb',
+                     'value': str(instance['root_gb']),
+                     'op': 'add'},
+                    {'path': '/instance_info/swap_mb',
+                     'value': str(self.flavor['swap']),
+                     'op': 'add'}]
+        patch = patcher.create(node).get_deploy_patch(
+                instance, self.image_meta, self.flavor)
+        self.assertEqual(sorted(expected), sorted(patch))
+
+    def test_pxe_get_cleanup_patch(self):
+        driver_info = {'pxe_deploy_kernel': 'fake-kernel-id',
+                       'pxe_deploy_ramdisk': 'fake-ramdisk-id'}
+        node = ironic_utils.get_test_node(driver='pxe_fake',
+                                          driver_info=driver_info)
+        instance = fake_instance.fake_instance_obj(self.ctx, node=node.uuid)
+        patch = patcher.create(node).get_cleanup_patch(instance, None,
+                                                       self.flavor)
+        expected = [{'path': '/driver_info/pxe_deploy_kernel',
+                     'op': 'remove'},
+                    {'path': '/driver_info/pxe_deploy_ramdisk',
+                     'op': 'remove'}]
+        self.assertEqual(sorted(expected), sorted(patch))
+
+    def test_pxe_get_cleanup_patch_no_flavor_kernel_ramdisk_ids(self):
+        self.flavor = ironic_utils.get_test_flavor(extra_specs={})
+        driver_info = {'pxe_deploy_kernel': 'fake-kernel-id',
+                       'pxe_deploy_ramdisk': 'fake-ramdisk-id'}
+        node = ironic_utils.get_test_node(driver='pxe_fake',
+                                          driver_info=driver_info)
+        instance = fake_instance.fake_instance_obj(self.ctx, node=node.uuid)
+        patch = patcher.create(node).get_cleanup_patch(instance, None,
+                                                       self.flavor)
+        self.assertEqual([], patch)
---
+++ patch-3/nova/virt/ironic/driver.py
@@ -0,0 +1,335 @@
+# coding=utf-8
+#
+# Copyright 2014 Red Hat, Inc.
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""
+A driver wrapping the Ironic API, such that Nova may provision
+bare metal resources.
+"""
+import logging as py_logging
+
+from ironicclient import exc as ironic_exception
+from oslo.config import cfg
+
+from nova.compute import power_state
+from nova.compute import task_states
+from nova import context as nova_context
+from nova import exception
+from nova.objects import flavor as flavor_obj
+from nova.objects import instance as instance_obj
+from nova.openstack.common import excutils
+from nova.openstack.common.gettextutils import _, _LW
+from nova.openstack.common import jsonutils
+from nova.openstack.common import log as logging
+from nova.openstack.common import loopingcall
+from nova.virt import driver as virt_driver
+from nova.virt import firewall
+from nova.virt.ironic import client_wrapper
+from nova.virt.ironic import ironic_states
+from nova.virt.ironic import patcher
+
+LOG = logging.getLogger(__name__)
+
+opts = [
+    cfg.IntOpt('api_version',
+               default=1,
+               help='Version of Ironic API service endpoint.'),
+    cfg.StrOpt('api_endpoint',
+               help='URL for Ironic API endpoint.'),
+    cfg.StrOpt('admin_username',
+               help='Ironic keystone admin name'),
+    cfg.StrOpt('admin_password',
+               help='Ironic keystone admin password.'),
+    cfg.StrOpt('admin_auth_token',
+               help='Ironic keystone auth token.'),
+    cfg.StrOpt('admin_url',
+               help='Keystone public API endpoint.'),
+    cfg.StrOpt('client_log_level',
+               help='Log level override for ironicclient. Set this in '
+                    'order to override the global "default_log_levels", '
+                    '"verbose", and "debug" settings.'),
+    cfg.StrOpt('pxe_bootfile_name',
+               help='This gets passed to Neutron as the bootfile dhcp '
+               'parameter when the dhcp_options_enabled is set.',
+               default='pxelinux.0'),
+    cfg.StrOpt('admin_tenant_name',
+               help='Ironic keystone tenant name.'),
+    cfg.ListOpt('instance_type_extra_specs',
+                default=[],
+                help='A list of additional capabilities corresponding to '
+                'instance_type_extra_specs for this compute '
+                'host to advertise. Valid entries are name=value, pairs '
+                'For example, "key1:val1, key2:val2"'),
+    cfg.IntOpt('api_max_retries',
+               default=60,
+               help=('How many retries when a request does conflict.')),
+    cfg.IntOpt('api_retry_interval',
+               default=2,
+               help=('How often to retry in seconds when a request '
+                     'does conflict')),
+    ]
+
+ironic_group = cfg.OptGroup(name='ironic',
+                            title='Ironic Options')
+
+CONF = cfg.CONF
+CONF.register_group(ironic_group)
+CONF.register_opts(opts, ironic_group)
+
+_FIREWALL_DRIVER = "%s.%s" % (firewall.__name__,
+                              firewall.NoopFirewallDriver.__name__)
+
+_POWER_STATE_MAP = {
+    ironic_states.POWER_ON: power_state.RUNNING,
+    ironic_states.NOSTATE: power_state.NOSTATE,
+    ironic_states.POWER_OFF: power_state.SHUTDOWN,
+}
+
+
+def map_power_state(state):
+    try:
+        return _POWER_STATE_MAP[state]
+    except KeyError:
+        LOG.warning(_("Power state %s not found.") % state)
+        return power_state.NOSTATE
+
+
+def validate_instance_and_node(icli, instance):
+    """Get and validate a node's uuid out of a manager instance dict.
+
+    The compute manager is meant to know the node uuid, so missing uuid
+    a significant issue - it may mean we've been passed someone elses data.
+
+    Check with the Ironic service that this node is still associated with
+    this instance. This catches situations where Nova's instance dict
+    contains stale data (eg, a delete on an instance that's already gone).
+
+    """
+    try:
+        return icli.call("node.get_by_instance_uuid", instance['uuid'])
+    except ironic_exception.NotFound:
+        raise exception.InstanceNotFound(instance_id=instance['uuid'])
+
+
+class IronicDriver(virt_driver.ComputeDriver):
+    """Hypervisor driver for Ironic - bare metal provisioning."""
+
+    capabilities = {"has_imagecache": False}
+
+    def __init__(self, virtapi, read_only=False):
+        super(IronicDriver, self).__init__(virtapi)
+
+        self.firewall_driver = firewall.load_driver(default=_FIREWALL_DRIVER)
+        extra_specs = {}
+        extra_specs["ironic_driver"] = \
+            "ironic.nova.virt.ironic.driver.IronicDriver"
+        for pair in CONF.ironic.instance_type_extra_specs:
+            keyval = pair.split(':', 1)
+            keyval[0] = keyval[0].strip()
+            keyval[1] = keyval[1].strip()
+            extra_specs[keyval[0]] = keyval[1]
+
+        self.extra_specs = extra_specs
+
+        icli_log_level = CONF.ironic.client_log_level
+        if icli_log_level:
+            level = py_logging.getLevelName(icli_log_level)
+            logger = py_logging.getLogger('ironicclient')
+            logger.setLevel(level)
+
+    def init_host(self, host):
+        """Initialize anything that is necessary for the driver to function.
+
+        :param host: the hostname of the compute host.
+
+        """
+        return
+
+    def get_hypervisor_type(self):
+        """Get hypervisor type."""
+        return 'ironic'
+
+    def get_hypervisor_version(self):
+        """Returns the version of the Ironic API service endpoint."""
+        return CONF.ironic.api_version
+
+    def instance_exists(self, instance):
+        """Checks the existence of an instance.
+
+        Checks the existence of an instance. This is an override of the
+        base method for efficiency.
+
+        :param instance: The instance object.
+        :returns: True if the instance exists. False if not.
+
+        """
+        icli = client_wrapper.IronicClientWrapper()
+        try:
+            validate_instance_and_node(icli, instance)
+            return True
+        except exception.InstanceNotFound:
+            return False
+
+    def list_instances(self):
+        """Return the names of all the instances provisioned.
+
+        :returns: a list of instance names.
+
+        """
+        icli = client_wrapper.IronicClientWrapper()
+        node_list = icli.call("node.list", associated=True)
+        context = nova_context.get_admin_context()
+        return [instance_obj.Instance.get_by_uuid(context,
+                                                  i.instance_uuid).name
+                for i in node_list]
+
+    def list_instance_uuids(self):
+        """Return the UUIDs of all the instances provisioned.
+
+        :returns: a list of instance UUIDs.
+
+        """
+        icli = client_wrapper.IronicClientWrapper()
+        node_list = icli.call("node.list", associated=True)
+        return list(set(n.instance_uuid for n in node_list))
+
+    def node_is_available(self, nodename):
+        """Confirms a Nova hypervisor node exists in the Ironic inventory.
+
+        :param nodename: The UUID of the node.
+        :returns: True if the node exists, False if not.
+
+        """
+        icli = client_wrapper.IronicClientWrapper()
+        try:
+            icli.call("node.get", nodename)
+            return True
+        except ironic_exception.NotFound:
+            return False
+
+    def get_available_nodes(self, refresh=False):
+        """Returns the UUIDs of all nodes in the Ironic inventory.
+
+        :param refresh: Boolean value; If True run update first. Ignored by
+            this driver.
+        :returns: a list of UUIDs
+
+        """
+        icli = client_wrapper.IronicClientWrapper()
+        node_list = icli.call("node.list")
+        nodes = [n.uuid for n in node_list]
+        LOG.debug("Returning %(num_nodes)s available node(s): %(nodes)s",
+                  dict(num_nodes=len(nodes), nodes=nodes))
+        return nodes
+
+    def get_available_resource(self, nodename):
+        """Retrieve resource information.
+
+        This method is called when nova-compute launches, and
+        as part of a periodic task that records the results in the DB.
+
+        :param nodename: the UUID of the node.
+        :returns: a dictionary describing resources.
+
+        """
+        icli = client_wrapper.IronicClientWrapper()
+        node = icli.call("node.get", nodename)
+        return self._node_resource(node)
+
+    def get_info(self, instance):
+        """Get the current state and resource usage for this instance.
+
+        If the instance is not found this method returns (a dictionary
+        with) NOSTATE and all resources == 0.
+
+        :param instance: the instance object.
+        :returns: a dictionary containing:
+            :state: the running state. One of :mod:`nova.compute.power_state`.
+            :max_mem:  (int) the maximum memory in KBytes allowed.
+            :mem:      (int) the memory in KBytes used by the domain.
+            :num_cpu:  (int) the number of CPUs.
+            :cpu_time: (int) the CPU time used in nanoseconds. Always 0 for
+                             this driver.
+
+        """
+        icli = client_wrapper.IronicClientWrapper()
+        try:
+            node = validate_instance_and_node(icli, instance)
+        except exception.InstanceNotFound:
+            return {'state': map_power_state(ironic_states.NOSTATE),
+                    'max_mem': 0,
+                    'mem': 0,
+                    'num_cpu': 0,
+                    'cpu_time': 0
+                    }
+
+        memory_kib = int(node.properties.get('memory_mb')) * 1024
+        return {'state': map_power_state(node.power_state),
+                'max_mem': memory_kib,
+                'mem': memory_kib,
+                'num_cpu': node.properties.get('cpus'),
+                'cpu_time': 0
+                }
+
+    def reboot(self, context, instance, network_info, reboot_type,
+               block_device_info=None, bad_volumes_callback=None):
+        """Reboot the specified instance.
+
+        :param context: The security context.
+        :param instance: The instance object.
+        :param network_info: Instance network information. Ignored by
+            this driver.
+        :param reboot_type: Either a HARD or SOFT reboot. Ignored by
+            this driver.
+        :param block_device_info: Info pertaining to attached volumes.
+            Ignored by this driver.
+        :param bad_volumes_callback: Function to handle any bad volumes
+            encountered. Ignored by this driver.
+
+        """
+        icli = client_wrapper.IronicClientWrapper()
+        node = validate_instance_and_node(icli, instance)
+        icli.call("node.set_power_state", node.uuid, 'reboot')
+
+    def power_off(self, instance):
+        """Power off the specified instance.
+
+        :param instance: The instance object.
+
+        """
+        # TODO(nobodycam): check the current power state first.
+        icli = client_wrapper.IronicClientWrapper()
+        node = validate_instance_and_node(icli, instance)
+        icli.call("node.set_power_state", node.uuid, 'off')
+
+    def power_on(self, context, instance, network_info,
+                 block_device_info=None):
+        """Power on the specified instance.
+
+        :param context: The security context.
+        :param instance: The instance object.
+        :param network_info: Instance network information. Ignored by
+            this driver.
+        :param block_device_info: Instance block device
+            information. Ignored by this driver.
+
+        """
+        # TODO(nobodycam): check the current power state first.
+        icli = client_wrapper.IronicClientWrapper()
+        node = validate_instance_and_node(icli, instance)
+        icli.call("node.set_power_state", node.uuid, 'on')
+
